78a8d22af9941ff2f58a389ab8d3fd9486879466
Seyptoo <39600961+Seyptoo@users.noreply.github.com>
2019-01-19 15:33:27 +0100

    Update NTLM.py

--- NTLM.py
+++ NTLM.py
@@ -1,40 +1,60 @@
+#coding:utf
+
 import sys
 import hashlib
 import binascii
 import threading
 import Queue
+import time
 
 class NTLM(threading.Thread):
-   def __init__(self, system_threads=35):
-      threading.Thread.__init__(self)
-      # Calling Thread variable.
-      try:
-         # We will test the arguments.
-         # To handle incorrect errors.
+	def __init__(self, system_threads=35):
+		threading.Thread.__init__(self)
+		self.threads = system_threads
+		# Calling Thread variable.
+		try:
+			# We will test the arguments.
+			# To handle incorrect errors.
 
-         self.LIST = sys.argv[1]
-         self.NTLM = sys.argv[2]
-   
-         # I create an exception for the error.
-         # For the use of the program.
-      except IndexError as e:
-         print e
+			self.LIST = sys.argv[1]
+			self.NTLM = sys.argv[2]
+	
+			# I create an exception for the error.
+			# For the use of the program.
+		except IndexError as e:
+			sys.exit(e)
 
-   def run(self):
-      q = Queue.Queue()
-      with open(self.LIST, "r") as file:
-         for online in file:
-            q.put(online.rstrip("\n\r"))
-         self.BertModel(q)       
+	def NTLModel(self, q):
+		while True:
+			if(self.NTLM.islower() == False):
+				self.NTLM = self.NTLM.lower()
 
-      for i in range(int(self.threads)):
-         wrapper = threading.Thread(target=self.BertModel, args=(i,q))
-         wrapper.setDaemon(True)
-         wrapper.start()
-         wrapper.join(600)
+			hash = hashlib.new('md4', q.get().encode('utf-16le')).digest()
+			if(binascii.hexlify(hash) == self.NTLM):
+				print "Cracked : %s" %(q.get())
 
-      q.join()
-      
+
+	def run(self):
+		"""
+		This function will handle
+		the thread system to speed up the
+		program and the list.
+		"""
+		if(self.LIST):
+			q = Queue.Queue()
+			with open(self.LIST, "r") as file:
+				for online in file:
+					q.put(online.strip("\n\r"))
+				self.NTLModel(q)		
+
+			for i in range(int(self.threads)):
+				wrapper = threading.Thread(target=self.NTLModel, args=(i, q))
+				wrapper.setDaemon(True)
+				wrapper.start()
+				wrapper.join(600)
+
+			q.join()
+		
 
 if __name__ == "__main__":
-   NTLM().start()
+	NTLM().start()
